18886258889cbf8fa6b80229236c153b01ece997
Hansanghyeon <dandan9509@gmail.com>
2020-07-23 01:55:34 +0900

    feat: Main Section type 1

---
+++ src/components/organisms/MainSection/type1/index.style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import {
+  Container as _Container,
+  Row as _Row,
+  Col as _Col,
+} from 'styled-bootstrap-grid';
+
+export const Container = {
+  Def: styled(_Container)``,
+};
+
+export const Row = {
+  Def: styled(_Row)`
+    margin-bottom: 2rem;
+  `,
+};
+
+export const Col = {
+  Def: styled(_Col)``,
+};
+
+export const Title = styled.div``;
