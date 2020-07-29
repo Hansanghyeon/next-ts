ed9084b444959a0c50f9209ebf01c8be03148dce
Hansanghyeon <dandan9509@gmail.com>
2020-07-29 10:24:31 +0900

    feat: remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
 
-import App from '@/components/App'
-import store from '@/features'
-import history from '@/utils/history'
+import App from '@/components/App';
+// import store from '@/features'
+import history from '@/utils/history';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('root'),
-)
+);
