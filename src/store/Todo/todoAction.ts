aaf393405b32c3e2affdf093bff74c24e724ec5e
Hansanghyeon <dandan9509@gmail.com>
2020-07-29 10:23:44 +0900

    feat: remove redux

--- src/store/Todo/todoAction.ts
+++
@@ -1,5 +0,0 @@
-export const CHANGE_INPUT = 'todo/CHANGE_INPUT';
-export const INSERT = 'todo/INSERT';
-export const TOGGLE_CHECK = 'todo/TOGGLE_CHECK';
-export const REMOVE = 'todo/REMOVE';
-export const TOGGLE_UPDATE = 'todo/TOGGLE_UPDATE';
