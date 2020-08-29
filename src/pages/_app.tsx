3642f8495879fb41c3ce33f15c702ce7a4c7bd48
Hansanghyeon <dandan9509@gmail.com>
2020-08-29 17:15:54 +0900

    chore: mobx 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,14 @@ import { AppProps } from 'next/app';
 // const name = '[Your Name]';
 export const siteTitle = 'Next.js Sample Website';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+App.getInitialProps = async () => {
+  return {
+    a: 1,
+  };
+};
+
+export default App;
